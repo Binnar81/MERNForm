d5c899ee4153394afaa77c48659beabcf8b09db8
sanjay binnar <72009615+Binnar81@users.noreply.github.com>
2024-03-13 16:51:32 +0530

    improve

--- reactform/src/FileUploadButton.js
+++ reactform/src/FileUploadButton.js
@@ -4,7 +4,7 @@ import { Button, Box } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 const FileUploadButton = ({ name, ...rest }) => {
-  const [field, , helpers] = useField(name);
+  const [field, meta, helpers] = useField(name);
   const [file, setFile] = useState(null);
 
   const handleFileChange = (event) => {
@@ -15,14 +15,15 @@ const FileUploadButton = ({ name, ...rest }) => {
 
   return (
     <Formik initialValues={{ [name]: null }} onSubmit={(values) => console.log(values)}>
-      {() => (
+      {({ isSubmitting }) => (
         <Form>
           <Box display="flex" alignItems="center">
-            <Button variant="contained" component="label" startIcon={<CloudUploadIcon />} {...rest}>
+            <Button variant="contained" component="label" startIcon={<CloudUploadIcon />} disabled={isSubmitting} {...rest}>
               Upload Attachment File
               <input type="file" onChange={handleFileChange} style={{ display: 'none' }} />
             </Button>
             {file && <span style={{ marginLeft: '1rem' }}>{file.name}</span>}
+            {meta.touched && meta.error && <span style={{ marginLeft: '1rem', color: 'red' }}>{meta.error}</span>}
           </Box>
         </Form>
       )}
